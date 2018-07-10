f8257841387ed73258d4072c6db94fea917a44cd
zdndd <dongningdella@gmail.com>
2018-07-10 12:44:01 +0800

    资源管理 - css 加载图片 字体  加载数据

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import './style.css';
+import vlogImg from './demo.png'
+function component() {
+  var element = document.createElement('div');
+
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = _.join(['Hel444lo', 'webpack2222'], ' ');
+  element.classList.add('hello');
+
+  // 将图像添加到我们现有的 div。
+   var myIcon = new Image();
+   myIcon.src = vlogImg;
+
+   element.appendChild(myIcon);
+
+
+  return element;
+}
+
+document.body.appendChild(component());
