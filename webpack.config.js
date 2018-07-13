e5330b47467ff56161ada6930b3e23f42c782b9e
zdndd <dongningdella@gmail.com>
2018-07-13 16:31:21 +0800

    热更替

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'production',
@@ -10,17 +10,19 @@ module.exports = {
   },
   entry: {
      app: './src/index.js',
-     print: './src/print.js'
    },
    devtool: 'inline-source-map',
    devServer: {
-     contentBase: './dist'
+     contentBase: './dist',
+     hot:true
    },
    plugins: [
       new CleanWebpackPlugin(['dist']),
       new HtmlWebpackPlugin({
        title: 'Output Management'
-     })
+      }),
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin()
    ],
   output: {
     filename: '[name].bundle.js',
