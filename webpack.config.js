10b56aadee83141375ec089fda301f0763a33fdf
zdndd <dongningdella@gmail.com>
2018-07-13 15:04:49 +0800

    管理输出-清理 /dist 文件夹

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 
 module.exports = {
@@ -8,7 +9,8 @@ module.exports = {
      print: './src/print.js'
    },
    plugins: [
-     new HtmlWebpackPlugin({
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
        title: 'Output Management'
      })
    ],
